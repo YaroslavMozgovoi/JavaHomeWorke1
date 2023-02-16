af8d04e0c7f6bb37e3cfaa08fbcd5bcb7342fba1
Yaroslav <mozgovoj.yaroslav@mail.ru>
2023-02-16 11:33:23 +0300

    Создал первый метод.Импортировал сканер.

--- triangularNumber.java
+++ triangularNumber.java
@@ -1,13 +1,18 @@
-
+package JavaHomeWorke1;
+import java.util.Scanner;
 public class triangularNumber {
     //Написать программу вычисления n-ого треугольного числа
+    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
+        int n = GetNumber("Введите целое число");
+        System.out.print(n);
         
     }
     
     //1.Ввод с консоли n-ого значения.
     public static int GetNumber(String text){
-
+        System.out.print(text);
+        return input.nextInt();
     }
 
     //2.Нахождение n-ого числа.
@@ -15,8 +20,8 @@ public class triangularNumber {
 
     }
 
-    //3.Вывод в консоль результата.
-    public static String Print(int triangular){
+    // //3.Вывод в консоль результата.
+    // public static String print(int triangular){
         
-    }
+    // }
 }
