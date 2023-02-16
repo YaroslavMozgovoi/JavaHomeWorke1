77303f5fcbc7f8546184121cb5172d755ce9ccc1
Yaroslav <mozgovoj.yaroslav@mail.ru>
2023-02-16 10:58:49 +0300

    Создал основные функции

---
+++ triangularNumber.java
@@ -0,0 +1,22 @@
+
+public class triangularNumber {
+    //Написать программу вычисления n-ого треугольного числа
+    public static void main(String[] args) {
+        
+    }
+    
+    //1.Ввод с консоли n-ого значения.
+    public static int GetNumber(String text){
+
+    }
+
+    //2.Нахождение n-ого числа.
+    public static void TriangularNumber(int number){
+
+    }
+
+    //3.Вывод в консоль результата.
+    public static String Print(int triangular){
+        
+    }
+}
